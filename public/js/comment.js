a5befea1331ba8c853b619daf6f5c0b1325d6dcd
bucknorris336 <buckblocker90gmail.com>
2023-04-21 19:45:23 -0400

    cant figure it out

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,11 +1,14 @@
+const postId = window.location.toString().split("/");
+console.log(postId);
+
 const commentFormHandler = async function (event) {
   event.preventDefault();
 
-  const postId = document.querySelector('input[name="post-id"]').value;
+  // const postId = document.querySelector('input[name="post-id"]').value;
   const body = document.querySelector('textarea[name="comment-body"]').value;
   // wrapped in one data set. All info will be availble to the server
   if (body) {
-    await fetch("/api/comment/postId", {
+    await fetch("/api/comment/", {
       method: "POST",
       body: JSON.stringify({
         postId,
